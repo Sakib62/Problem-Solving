5cd4d69ceb87112fd427dc354c438ae2dd9d472f
Sakib62 <sakib62.coder@gmail.com>
2022-12-01 20:27:05 +0600

    updated

--- CSES/SortingSearching/1084.cpp
+++ CSES/SortingSearching/1084.cpp
@@ -22,25 +22,26 @@ int main() {
         cin >> n >> m >> k;
         int a[n], b[m];
         for(int i = 0; i < n; i++) {
-        	cin >> a[i];
+        	cin >> a[i];//desired apartment size
         }
         sort(a, a + n);
         for(int i = 0; i < m; i++) {
-        	cin >> b[i];
+        	cin >> b[i];//actual apartment size
         }
         sort(b, b + m);
+        //both array sorted
         int cnt = 0;
         for(int i = 0, j = 0; i < n && j < m; i++) {
-        	if(a[i] + k >= b[j] && a[i] - k <= b[j]) {
-        		cnt++;
-        		j++;
+        	if(a[i] + k >= b[j] && a[i] - k <= b[j]) { //ith applicant's desired size is fulfilled by jth apartment
+        		cnt++;//hence, increment counter
+        		j++;//move on to next apartment, outer loop will move on to next applicant
         	}
-        	else if(b[j] > a[i] + k) continue;
-        	else {
-        		while(j < m && b[j] < a[i] - k) {
+        	else if(b[j] > a[i] + k) continue;//j'th apartment is much bigger than i'th applicant's need. Hence, i++, moving on to see if i+1'th applicant's need match
+        	else {//j'th apartment is much smaller than i'th applicant's need, so move on to next apartment, applicant remains same
+        		while(j < m && b[j] < a[i] - k) {//as long as apartment size is smaller than i'th applicant's need
         			j++;
-        		}
-        		i--;
+        		}//loop breaks, either no more apartment or found a match for i'th applicant
+        		i--;//outer loop will increment thus skips the current matched applicant, so first decrement
         	}
         }
         cout << cnt << "\n";
