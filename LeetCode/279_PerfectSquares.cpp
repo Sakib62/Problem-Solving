af923e47ce6bfb447de5260a3da5a412611900b5
Sakibul Islam <sakib62.coder@gmail.com>
2024-02-09 00:04:30 +0600

    Update 279_PerfectSquares.cpp

--- LeetCode/279_PerfectSquares.cpp
+++ LeetCode/279_PerfectSquares.cpp
@@ -13,6 +13,13 @@ public:
     
     int numSquares(int n) {
         vector <int> dp(n + 1, INT_MAX);
-        return fun(n, dp);
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);   
+            }
+        }
+        return dp[n];
+        //return fun(n, dp);
     }
 };
